b92ca1d9cb74e08e491aa129685f833bdb85a6db
Arno Purboyo <113575056+Arno63@users.noreply.github.com>
2023-08-24 11:32:00 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 
 new Vue({
   router,
   store,
+  Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
+ 
