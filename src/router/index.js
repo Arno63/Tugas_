2f437d58aad60045bbcd6b165c179cc26f2b3f75
Arno Purboyo <113575056+Arno63@users.noreply.github.com>
2023-08-24 11:21:28 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import ForgotPassword from '../components/ForgotPassword.vue'
+import Dashboard from '../components/Dashboard.vue'
+import SignUp from '../views/SignUp.vue'
+import DataLaporan from '../components/DataLaporan.vue'
+import Reset from '../components/Reset.vue'
+import FormIzin from "../views/FormIzin.vue"
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/formizin',
+    name: 'formizin',
+    component: FormIzin
+  },
+  {
+    path: '/reset',
+    name: 'reset',
+    component: Reset
+  },
+  {
+    path: '/datalaporan',
+    name: 'datalaporan',
+    component: DataLaporan
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/forgotpassword',
+    name: 'forgotpassword',
+    component: ForgotPassword
+  },
+  {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
